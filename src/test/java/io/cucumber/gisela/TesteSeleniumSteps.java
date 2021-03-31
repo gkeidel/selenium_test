a5982b82da6d8a04fd81e6d080098296cf59f6be
Gisela Keidel <giselakeidel@gmail.com>
2021-03-31 11:45:12 -0300

    primeira parte do teste concluida com sucesso

---
+++ src/test/java/io/cucumber/gisela/TesteSeleniumSteps.java
@@ -0,0 +1,64 @@
+package io.cucumber.gisela;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.cucumber.java.pt.*;
+
+public class TesteSeleniumSteps {
+
+  public WebDriver driver;
+  
+  @Dado("que entro no site da tricentis")
+  public void que_entro_no_site_da_tricentis() {
+    System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+
+    driver = new ChromeDriver();
+    driver.get("http://sampleapp.tricentis.com/101/app.php");
+    driver.manage().window().maximize();
+  }
+
+  @Dado("preencho o formulario")
+  public void preencho_o_campo_make() {
+    Select make = new Select(driver.findElement(By.id("make")));
+    make.selectByVisibleText("BMW");
+    Select model = new Select(driver.findElement(By.id("model")));
+    model.selectByVisibleText("Motorcycle");
+    WebElement cylinder = driver.findElement(By.id("cylindercapacity"));
+    cylinder.sendKeys("1998");
+    WebElement engine = driver.findElement(By.id("engineperformance"));
+    engine.sendKeys("170");
+    WebElement dateBox = driver.findElement(By.id("dateofmanufacture"));
+    dateBox.sendKeys("01/30/2021");
+    Select seats = new Select(driver.findElement(By.id("numberofseats")));
+    seats.selectByVisibleText("4");
+    WebElement hand_drive = driver.findElement(By.cssSelector(".ideal-radio"));
+    hand_drive.click();
+    Select seats_motorcycle = new Select(driver.findElement(By.id("numberofseatsmotorcycle")));
+    seats_motorcycle.selectByVisibleText("2");
+    Select fuel = new Select(driver.findElement(By.id("fuel")));
+    fuel.selectByVisibleText("Gas");
+    WebElement payload = driver.findElement(By.id("payload"));
+    payload.sendKeys("208");
+    WebElement weight = driver.findElement(By.id("totalweight"));
+    weight.sendKeys("1525");
+    WebElement price = driver.findElement(By.id("listprice"));
+    price.sendKeys("88000");
+    WebElement plate_number = driver.findElement(By.id("licenseplatenumber"));
+    plate_number.sendKeys("KDI-7876");
+    WebElement annual_mileage = driver.findElement(By.id("annualmileage"));
+    annual_mileage.sendKeys("8000");
+  }
+  
+  @Entao("clico em next")
+  public void clico_em_next() {
+    WebElement botaoNext = driver.findElement(By.id("nextenterinsurantdata"));
+    botaoNext.click();
+  }
+}
---
+++ src/test/resources/io/cucumber/gisela/TesteSelenium.feature
@@ -0,0 +1,24 @@
+# language: pt
+
+Funcionalidade: Teste Gama Academy 
+  Teste individual para verificar o conteúdo aprendido
+
+  Cenario: Entrar no site da Tricentis e rodar alguns testes
+  Dado que entro no site da tricentis
+  E preencho o formulario
+  Entao clico em next
+
+
+# Entrar no site http://sampleapp.tricentis.com/101/app.php
+
+# Preencher o formulário, aba “Enter Vehicle Data” e pressione next
+
+# Preencher o formulário, aba “Enter Insurant Data” e pressione next
+
+# Preencher o formulário, aba “Enter Product Data” e pressione next
+
+# Preencher o formulário, aba “Select Price Option” e pressione next
+
+# Preencher o formulário, aba “Send Quote” e pressione Send
+
+# Verificar a mensagem “Sending e-mail success!” na tela
